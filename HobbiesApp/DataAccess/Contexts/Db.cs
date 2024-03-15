bf5339d7fc88599be51e35f6e6c67e96c904bf24
Asrin_Degirmenci <asrin.degirmenci@ug.bilkent.edu.tr>
2024-03-15 12:48:46 +0300

    phase 1

---
+++ HobbiesApp/DataAccess/Contexts/Db.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Contexts
+{
+    public class Db : DbContext
+    {
+        public DbSet<Hobby> Hobbies { get; set; }
+        public DbSet<Games> Games { get; set; }
+        public Db(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
